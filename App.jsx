255c022eeecaa3a6bbc0b2da3795bf4b2b03c8a3
gzippedfrog <gzippedfrog@outlook.com>
2021-12-18 22:23:18 +0300

    added react-navigation

--- App.jsx
+++ App.jsx
@@ -1,7 +1,9 @@
 import { StatusBar } from "expo-status-bar";
-import React, { useState } from "react";
-import { BottomNavigation } from "react-native-paper";
+import React from "react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { NavigationContainer } from "@react-navigation/native";
+import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
@@ -10,40 +12,56 @@ import { store, persistor } from "./redux/store";
 import HomeScreen from "./screens/HomeScreen";
 import SavedScreen from "./screens/SavedScreen";
 
-const App = () => {
-  const [index, setIndex] = useState(0);
-  const [routes] = useState([
-    { key: "home", title: "Home", icon: "home" },
-    { key: "saved", title: "Saved", icon: "bookmark" },
-  ]);
-  const HomeRoute = () => <HomeScreen />;
-  const SavedRoute = () => <SavedScreen />;
-  const renderScene = BottomNavigation.SceneMap({
-    home: HomeRoute,
-    saved: SavedRoute,
-  });
+const Tab = createMaterialBottomTabNavigator();
 
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
+const App = () => (
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <NavigationContainer>
         <StatusBar style="light" backgroundColor="#6200ee" />
         <SafeAreaProvider>
           <SafeAreaView
             style={{
               height: "100%",
-              backgroundColor: "#eee",
             }}
           >
-            <BottomNavigation
-              navigationState={{ index, routes }}
-              onIndexChange={setIndex}
-              renderScene={renderScene}
-            />
+            <Tab.Navigator
+              barStyle={{
+                backgroundColor: "#6200ee",
+              }}
+            >
+              <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons
+                      name="home"
+                      color={color}
+                      size={26}
+                    />
+                  ),
+                }}
+              />
+              <Tab.Screen
+                name="Saved"
+                component={SavedScreen}
+                options={{
+                  tabBarIcon: ({ color }) => (
+                    <MaterialCommunityIcons
+                      name="bookmark"
+                      color={color}
+                      size={26}
+                    />
+                  ),
+                }}
+              />
+            </Tab.Navigator>
           </SafeAreaView>
         </SafeAreaProvider>
-      </PersistGate>
-    </Provider>
-  );
-};
+      </NavigationContainer>
+    </PersistGate>
+  </Provider>
+);
 
 export default App;
--- redux/actions.js
+++ redux/actions.js
@@ -1,10 +1,4 @@
-import {
-  FETCH_RESULTS,
-  HIDE_BAR,
-  REMOVE_DEFINITION,
-  SAVE_DEFINITION,
-  SHOW_BAR,
-} from "./types";
+import { FETCH_RESULTS, HIDE_BAR, SAVE_DEFINITION, SHOW_BAR } from "./types";
 
 const key = "0a97f0e1-ac7e-41ca-9422-f61d039223b9";
 const url = "https://www.dictionaryapi.com/api/v3/references/collegiate/json/";
